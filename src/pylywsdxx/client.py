a06e6637635e7ebb2a7b556d31253b2ab5a6bc02
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2023-04-16 13:34:02 +0200

    code moved

--- src/pylywsdxx/client.py
+++ src/pylywsdxx/client.py
@@ -355,13 +355,3 @@ class Lywsd03client(Lywsd02client):
             Nothing
         """
         return
-
-
-def ble_reset(delay=5.0):
-    """Reset the bluetooth hardware"""
-    # Have you tried turning it off and on again?
-    os.system("/usr/bin/bluetoothctl power off")
-    time.sleep(delay)
-    os.system("/usr/bin/bluetoothctl power on")
-    # if all else fails...
-    # os.system("/usr/bin/sudo /usr/bin/systemctl restart bluetooth.service")
