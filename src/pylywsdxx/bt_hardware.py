6fe24089cbe7ad3ac68283798bb5e29ab9c55f88
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-05-18 13:54:23 +0200

    update

--- src/pylywsdxx/bt_hardware.py
+++ src/pylywsdxx/bt_hardware.py
@@ -5,18 +5,23 @@ import time
 
 
 def ble_reset(delay=5.0):
-    """Reset the bluetooth hardware"""
+    """Reset the bluetooth hardware by switching the power OFF and ON again.
+
+    Args:
+        delay (float): number of seconds between switching OFF and ON
+
+    Returns:
+        None
+    """
 
     # Have you tried turning it off and on again?
     args = ["/usr/bin/bluetoothctl", "power", "off"]
     _exit_code = subprocess.check_output(args, shell=False)  # nosec B603
-    # os.system("/usr/bin/bluetoothctl power off")
 
     time.sleep(delay)
 
     args = ["/usr/bin/bluetoothctl", "power", "on"]
     _exit_code = subprocess.check_output(args, shell=False)  # nosec B603
-    # os.system("/usr/bin/bluetoothctl power on")
 
     # if all else fails...
     # os.system("/usr/bin/sudo /usr/bin/systemctl restart bluetooth.service")
