65ac4232f2bd404eb9dd073c9c1b0e83cd936850
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-05-19 09:39:04 +0200

    Revert "order of functions changed"
    This reverts commit ee3288a49103e0f90d4ce978ecf25518867c56d5.

--- src/pylywsdxx/client.py
+++ src/pylywsdxx/client.py
@@ -53,6 +53,124 @@ class Lywsd02client:  # pylint: disable=R0902
         self._history_data = collections.OrderedDict()
         self._context_depth = 0
 
+    @contextlib.contextmanager
+    def connect(self):
+        """Handle device connecting and disconnecting"""
+        if self._context_depth == 0:
+            if self.debug:
+                print(f"|-> Connecting to {self._mac}")
+            self._peripheral.connect(addr=self._mac, timeout=self._notification_timeout)
+            # TO DO: consider to catch connection errors here
+            # kimnaty.kimnaty[1372]: *** While talking to room 1.1 (A4:C1:38:6F:E7:CA) an error occured on 2023-04-15 18:20:35
+            # kimnaty.kimnaty[1372]:      -btle- Timed out while trying to connect to peripheral A4:C1:38:6F:E7:CA, addr type: public, interface None, >
+        self._context_depth += 1
+        try:
+            yield self
+        except Exception as her:
+            print(f"(pylywsdxx.client) An exception of type {type(her).__name__} occured")
+        finally:
+            self._context_depth -= 1
+            if self._context_depth == 0:
+                if self.debug:
+                    print(f"|-< Disconnecting from {self._mac}")
+                self._peripheral.disconnect()
+
+    @property
+    def temperature(self):
+        return self.data.temperature
+
+    @property
+    def humidity(self):
+        return self.data.humidity
+
+    @property
+    def data(self):
+        self._get_sensor_data()
+        return self._data
+
+    @property
+    def units(self):
+        with self.connect():
+            ch = self._peripheral.getCharacteristics(uuid=UUID_UNITS)[0]
+            value = ch.read()
+        return self.UNITS[value]
+
+    @units.setter
+    def units(self, value):
+        if value.upper() not in self.UNITS_CODES:
+            raise ValueError(
+                f"(pylywsdxx.client.py) Units value must be one of {self.UNITS_CODES.keys()}"
+            )
+
+        with self.connect():
+            ch = self._peripheral.getCharacteristics(uuid=UUID_UNITS)[0]
+            ch.write(self.UNITS_CODES[value.upper()], withResponse=True)
+
+    @property
+    def battery(self):
+        with self.connect():
+            ch = self._peripheral.getCharacteristics(uuid=UUID_BATTERY)[0]
+            value = ch.read()
+        return ord(value)
+
+    @property
+    def time(self):
+        with self.connect():
+            ch = self._peripheral.getCharacteristics(uuid=UUID_TIME)[0]
+            value = ch.read()
+        if len(value) == 5:
+            ts, tz_offset = struct.unpack("Ib", value)
+        else:
+            ts = struct.unpack("I", value)[0]
+            tz_offset = 0
+        return datetime.fromtimestamp(ts), tz_offset
+
+    @time.setter
+    def time(self, dt: datetime):
+        data = struct.pack("Ib", int(dt.timestamp()), self.tz_offset)
+        with self.connect():
+            ch = self._peripheral.getCharacteristics(uuid=UUID_TIME)[0]
+            ch.write(data, withResponse=True)
+
+    @property
+    def tz_offset(self):
+        if self._tz_offset is not None:
+            return self._tz_offset
+        if time.daylight:
+            return -time.altzone // 3600
+        return -time.timezone // 3600
+
+    @tz_offset.setter
+    def tz_offset(self, tz_offset: int):
+        self._tz_offset = tz_offset
+
+    @property
+    def history_index(self):
+        with self.connect():
+            ch = self._peripheral.getCharacteristics(uuid=UUID_RECORD_IDX)[0]
+            value = ch.read()
+        _idx = 0 if len(value) == 0 else struct.unpack_from("I", value)
+        return _idx
+
+    @history_index.setter
+    def history_index(self, value):
+        with self.connect():
+            ch = self._peripheral.getCharacteristics(uuid=UUID_RECORD_IDX)[0]
+            ch.write(struct.pack("I", value), withResponse=True)
+
+    @property
+    def num_stored_entries(self):
+        with self.connect():
+            ch = self._peripheral.getCharacteristics(uuid=UUID_NUM_RECORDS)[0]
+            value = ch.read()
+        total_records, current_records = struct.unpack_from("II", value)
+        return total_records, current_records
+
+    @property
+    def history_data(self):
+        self._get_history_data()
+        return self._history_data
+
     def _get_sensor_data(self):
         with self.connect():
             self._subscribe(UUID_DATA, self._process_sensor_data)
@@ -101,126 +219,6 @@ class Lywsd02client:  # pylint: disable=R0902
 
         self._history_data[idx] = [ts, min_temp, min_hum, max_temp, max_hum]
 
-    @contextlib.contextmanager
-    def connect(self):
-        """Handle device connecting and disconnecting"""
-        if self._context_depth == 0:
-            if self.debug:
-                print(f"|-> Connecting to {self._mac}")
-            self._peripheral.connect(addr=self._mac, timeout=self._notification_timeout)
-            # TO DO: consider to catch connection errors here
-            # kimnaty.kimnaty[74525]: *** While talking to room 0.5 (A4:C1:38:99:AC:4D) error
-            #                         (btle.py) Timed out while trying to connect to
-            #                         peripheral ...blabla... of type BTLEConnectTimeout occured
-            #                         on ___
-        self._context_depth += 1
-        try:
-            yield self
-        except Exception as her:
-            print(f"(pylywsdxx.client) An exception of type {type(her).__name__} occured")
-        finally:
-            self._context_depth -= 1
-            if self._context_depth == 0:
-                if self.debug:
-                    print(f"|-< Disconnecting from {self._mac}")
-                self._peripheral.disconnect()
-
-    @property
-    def battery(self):
-        with self.connect():
-            ch = self._peripheral.getCharacteristics(uuid=UUID_BATTERY)[0]
-            value = ch.read()
-        return ord(value)
-
-    @property
-    def humidity(self):
-        return self.data.humidity
-
-    @property
-    def temperature(self):
-        return self.data.temperature
-
-    @property
-    def data(self):
-        self._get_sensor_data()
-        return self._data
-
-    @property
-    def num_stored_entries(self):
-        with self.connect():
-            ch = self._peripheral.getCharacteristics(uuid=UUID_NUM_RECORDS)[0]
-            value = ch.read()
-        total_records, current_records = struct.unpack_from("II", value)
-        return total_records, current_records
-
-    @property
-    def history_data(self):
-        self._get_history_data()
-        return self._history_data
-
-    @property
-    def units(self):
-        with self.connect():
-            ch = self._peripheral.getCharacteristics(uuid=UUID_UNITS)[0]
-            value = ch.read()
-        return self.UNITS[value]
-
-    @units.setter
-    def units(self, value):
-        if value.upper() not in self.UNITS_CODES:
-            raise ValueError(
-                f"(pylywsdxx.client.py) Units value must be one of {self.UNITS_CODES.keys()}"
-            )
-
-        with self.connect():
-            ch = self._peripheral.getCharacteristics(uuid=UUID_UNITS)[0]
-            ch.write(self.UNITS_CODES[value.upper()], withResponse=True)
-
-    @property
-    def time(self):
-        with self.connect():
-            ch = self._peripheral.getCharacteristics(uuid=UUID_TIME)[0]
-            value = ch.read()
-        if len(value) == 5:
-            ts, tz_offset = struct.unpack("Ib", value)
-        else:
-            ts = struct.unpack("I", value)[0]
-            tz_offset = 0
-        return datetime.fromtimestamp(ts), tz_offset
-
-    @time.setter
-    def time(self, dt: datetime):
-        data = struct.pack("Ib", int(dt.timestamp()), self.tz_offset)
-        with self.connect():
-            ch = self._peripheral.getCharacteristics(uuid=UUID_TIME)[0]
-            ch.write(data, withResponse=True)
-
-    @property
-    def tz_offset(self):
-        if self._tz_offset is not None:
-            return self._tz_offset
-        if time.daylight:
-            return -time.altzone // 3600
-        return -time.timezone // 3600
-
-    @tz_offset.setter
-    def tz_offset(self, tz_offset: int):
-        self._tz_offset = tz_offset
-
-    @property
-    def history_index(self):
-        with self.connect():
-            ch = self._peripheral.getCharacteristics(uuid=UUID_RECORD_IDX)[0]
-            value = ch.read()
-        _idx = 0 if len(value) == 0 else struct.unpack_from("I", value)
-        return _idx
-
-    @history_index.setter
-    def history_index(self, value):
-        with self.connect():
-            ch = self._peripheral.getCharacteristics(uuid=UUID_RECORD_IDX)[0]
-            ch.write(struct.pack("I", value), withResponse=True)
-
 
 class Lywsd03client(Lywsd02client):
     """Class to communicate with LYWSD03MMC devices."""
@@ -240,6 +238,38 @@ class Lywsd03client(Lywsd02client):
     def __init__(self, mac, notification_timeout=12.3, debug=False):
         super().__init__(mac=mac, notification_timeout=notification_timeout, debug=debug)
         self._latest_record = None
+
+    def _process_sensor_data(self, data):
+        """Process the sensor data.
+
+        Args:
+            data (struct): struct containing sensor data
+        """
+        temperature, humidity, voltage = struct.unpack_from("<hBh", data)
+        temperature /= 100
+        voltage /= 1000
+        battery = round(((voltage - 2.1) / (3.4 - 2.1) * 100), 1)
+        """float: Estimate percentage of the battery charge remaining
+        CR2025 / CR2032 maximum theoretical voltage = 3.4 V
+        ref. Table 1;
+         CR2025: https://www.farnell.com/datasheets/1496883.pdf
+         CR2032: https://www.farnell.com/datasheets/1496885.pdf
+        Lowest voltage for these batteries is 2.0 V but the BT radio
+        on most devices will stop working when below 2.3 V (YMMV).
+        """
+        self._data = SensorData(
+            temperature=temperature, humidity=humidity, battery=battery, voltage=voltage
+        )
+
+    @property
+    def battery(self):
+        """
+        Battery data comes along with the temperature and humidity data, so just get it from there.
+
+        Returns:
+             guestimate of battery percentage
+        """
+        return self.data.battery
 
     def _get_history_data(self):
         # FIXME: Get the time the device was first run
@@ -265,28 +295,6 @@ class Lywsd03client(Lywsd02client):
                 if self._latest_record and self._latest_record >= expected_end:
                     break
 
-    def _process_sensor_data(self, data):
-        """Process the sensor data.
-
-        Args:
-            data (struct): struct containing sensor data
-        """
-        temperature, humidity, voltage = struct.unpack_from("<hBh", data)
-        temperature /= 100
-        voltage /= 1000
-        battery = round(((voltage - 2.1) / (3.4 - 2.1) * 100), 1)
-        """float: Estimate percentage of the battery charge remaining
-        CR2025 / CR2032 maximum theoretical voltage = 3.4 V
-        ref. Table 1;
-         CR2025: https://www.farnell.com/datasheets/1496883.pdf
-         CR2032: https://www.farnell.com/datasheets/1496885.pdf
-        Lowest voltage for these batteries is 2.0 V but the BT radio
-        on most devices will stop working when below 2.3 V (YMMV).
-        """
-        self._data = SensorData(
-            temperature=temperature, humidity=humidity, battery=battery, voltage=voltage
-        )
-
     def _process_history_data(self, data):
         (idx, ts, max_temp, max_hum, min_temp, min_hum) = struct.unpack_from("<IIhBhB", data)
 
@@ -303,16 +311,6 @@ class Lywsd03client(Lywsd02client):
         if not self.enable_history_progress:
             return
         print(f"|-- {ts}: {min_temp} to {max_temp}")
-
-    @property
-    def battery(self):
-        """
-        Battery data comes along with the temperature and humidity data, so just get it from there.
-
-        Returns:
-             guestimate of battery percentage
-        """
-        return self.data.battery
 
     @property
     def start_time(self):
