bc7db5643f642ba2f420995422856f2ceac4833f
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-05-19 09:39:07 +0200

    Revert "added for debugging"
    This reverts commit e2d93b59c31ca8c2a0129766fada42b7f428284d.

--- src/pylywsdxx/bt_hardware.py
+++ src/pylywsdxx/bt_hardware.py
@@ -4,11 +4,10 @@ import subprocess  # nosec B404
 import time
 
 
-def ble_reset(delay=5.0, debug=False):
+def ble_reset(delay=5.0):
     """Reset the bluetooth hardware by switching the power OFF and ON again.
 
     Args:
-        debug (bool): for debugging purposes
         delay (float): number of seconds between switching OFF and ON
 
     Returns:
@@ -18,15 +17,11 @@ def ble_reset(delay=5.0, debug=False):
     # Have you tried turning it off and on again?
     args = ["/usr/bin/bluetoothctl", "power", "off"]
     _exit_code = subprocess.check_output(args, shell=False)  # nosec B603
-    if debug:
-        print(f"BT pwr off result: {_exit_code}")
 
     time.sleep(delay)
 
     args = ["/usr/bin/bluetoothctl", "power", "on"]
     _exit_code = subprocess.check_output(args, shell=False)  # nosec B603
-    if debug:
-        print(f"BT pwr on result: {_exit_code}")
 
     # if all else fails...
     # os.system("/usr/bin/sudo /usr/bin/systemctl restart bluetooth.service")
