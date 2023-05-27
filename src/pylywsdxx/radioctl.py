593e6144d8c602c60cce7bb32faa83bafc816306
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-05-27 18:50:43 +0200

    updated

--- src/pylywsdxx/radioctl.py
+++ src/pylywsdxx/radioctl.py
@@ -20,4 +20,5 @@ def ble_reset(delay=5.0):
     args = ["/usr/bin/bluetoothctl", "power", "on"]
     _exit_code = subprocess.check_output(args, shell=False)  # nosec B603
 
-    # if all else fails...  # os.system("/usr/bin/sudo /usr/bin/systemctl restart bluetooth.service")
+    # if all else fails...
+    # os.system("/usr/bin/sudo /usr/bin/systemctl restart bluetooth.service")
