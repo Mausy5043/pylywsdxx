34b380b40655f90b4029cb7166e7c15268c1798c
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-05-29 10:40:14 +0200

    report exit codes

--- src/pylywsdxx/radioctl.py
+++ src/pylywsdxx/radioctl.py
@@ -9,20 +9,20 @@ warnings.filterwarnings("always", category=RuntimeWarning)
 
 def ble_reset(delay=5.0, debug=False):
     """Reset the bluetooth hardware"""
-
     warnings.warn("Resetting BT-radio.", RuntimeWarning, stacklevel=2)
 
     # Have you tried turning it off and on again?
     args = ["/usr/bin/bluetoothctl", "power", "off"]
-    _exit_code = subprocess.check_output(args, shell=False)  # nosec B603
+    _exit_code_on = subprocess.check_output(args, shell=False)  # nosec B603
     if debug:
-        print(f"Radio off ({_exit_code})")
+        print(f"Radio off ({_exit_code_on})")
 
     time.sleep(delay)
     args = ["/usr/bin/bluetoothctl", "power", "on"]
-    _exit_code = subprocess.check_output(args, shell=False)  # nosec B603
+    _exit_code_off = subprocess.check_output(args, shell=False)  # nosec B603
     if debug:
-        print(f"Radio on ({_exit_code})")
+        print(f"Radio on ({_exit_code_off})")
 
     # if all else fails...
     # os.system("/usr/bin/sudo /usr/bin/systemctl restart bluetooth.service")
+    return _exit_code_on, _exit_code_off
