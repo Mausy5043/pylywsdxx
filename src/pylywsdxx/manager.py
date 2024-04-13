a5f7ac2a3d2c02cff69b60e6563bbbf23822b266
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-04-13 14:33:23 +0200

    update

--- src/pylywsdxx/manager.py
+++ src/pylywsdxx/manager.py
@@ -25,8 +25,8 @@ class PyLyManager:
     """
 
     def __init__(self, debug: bool = False) -> None:
+        self.device_db: dict[str, dict] = {}
         self.mgr_debug: bool = debug
-        self.devices: dict[str, dict] = {}
         self.mgr_notification_timeout: float = 11.0
         self.mgr_reusable: bool = False
 
@@ -55,13 +55,13 @@ class PyLyManager:
                 reusable=self.mgr_reusable,
                 debug=self.mgr_debug,
             )
-        self.devices[name] = {
+        self.device_db[name] = {
             "mac": mac,
             "object": _object,
             "state": {"mac": mac, "name": name, "quality": 100},
         }
 
-    def get_state(self, name: str) -> dict[str, Any]:
+    def get_state(self, name: str):
         """Return the last known state of the given device.
 
         Args:
@@ -70,9 +70,9 @@ class PyLyManager:
         Returns:
             dict containing state information
         """
-        return self.devices[name]["state"]
+        return self.device_db[name]["state"], self.device_db[name]["quality"]
 
-    def update(self, name: str) -> None:
+    def update(self, name: str) -> bool:
         """Update the device's state information.
 
         Args:
@@ -81,17 +81,20 @@ class PyLyManager:
         Returns:
             Nothing
         """
-        device_data = self.devices[name]["object"].data
-        self.devices[name]["state"]["temperature"] = device_data.temperature
-        self.devices[name]["state"]["humidity"] = device_data.humidity
-        self.devices[name]["state"]["voltage"] = device_data.voltage
-        self.devices[name]["state"]["datetime"] = dt.datetime.now()
-        self.devices[name]["state"]["epoch"] = int(dt.datetime.now().timestamp())
+        device_data: Any = self.device_db[name]["object"].data
+        self.device_db[name]["state"]["temperature"] = device_data.temperature
+        self.device_db[name]["state"]["humidity"] = device_data.humidity
+        self.device_db[name]["state"]["voltage"] = device_data.voltage
+        self.device_db[name]["state"]["datetime"] = dt.datetime.now()
+        self.device_db[name]["state"]["epoch"] = int(dt.datetime.now().timestamp())
+        self.device_db[name]["state"]["quality"] = 100
+        return True
 
-    def update_all(self) -> None:
-        """Update the state of all devices known to the manager."""
-        for device_to_update in self.devices:
-            self.update(name=device_to_update)
+    def update_all(self) -> bool:
+        """Update the state of all device_db known to the manager."""
+        for device_to_update in self.device_db:
+            _succes = self.update(name=device_to_update)
+        return _succes
 
 
 class RepeatedTimer(object):
