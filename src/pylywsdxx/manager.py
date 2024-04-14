f1bc1d216a8267aa8b4a4e565525d4413a099d2a
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-04-14 09:22:11 +0200

    protect against execution when importing

--- src/pylywsdxx/manager.py
+++ src/pylywsdxx/manager.py
@@ -126,10 +126,10 @@ class RepeatedTimer(object):
 def hello(name):
     print("Hello %s!" % name)
 
-
-print("starting...")
-rt = RepeatedTimer(1, hello, "World")  # it auto-starts, no need of rt.start()
-try:
-    time.sleep(5)  # your long-running job goes here...
-finally:
-    rt.stop()  # better in a try/finally block to make sure the program ends!
+if __name__ == "__main__":
+    print("starting...")
+    rt = RepeatedTimer(1, hello, "World")  # it auto-starts, no need of rt.start()
+    try:
+        time.sleep(5)  # your long-running job goes here...
+    finally:
+        rt.stop()  # better in a try/finally block to make sure the program ends!
