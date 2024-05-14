e315fb37822e45136816074fa6adf52de958a5f9
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-05-14 09:07:04 +0200

    INITIAL commit

---
+++ examples/managedlywsd.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import argparse
+
+import pylywsdxx as pyly  # noqa
+
+# In these examples we don't care about pylint's W0718
+# pylint: disable=broad-exception-caught
+
+parser = argparse.ArgumentParser()
+parser.add_argument("mac", help="MAC address of LYWSDxx device", nargs="+")
+args = parser.parse_args()
+
+mymanager = pyly.PyLyManager()
+
+# Tell the manager which devices you want to subscribe to
+for mac in args.mac:
+    try:
+        client = mymanager.subscribe_to(mac)
+        print(f"Added: {mac}")
+    except (Exception,) as e:
+        print(f"An exception of type {type(e).__name__} occured when trying to subscribe to {mac}")
+        print(e)
+
+# Ask the manager to fetch the latest state of all subscribed devices
+try:
+    mymanager.update_all()
+except (Exception,) as e:
+    print(f"An exception of type {type(e).__name__} occured")
+    print(e)
+
+#
+for mac in args.mac:
+    try:
+        print(f"Fetching data for {mac}")
+        data = mymanager.get_state_of(mac)
+        print(f"Temperature: {data["temperature"]}°C")
+        print(f"Humidity: {data["humidity"]}%")
+        print(f"Battery: {data["battery"]}% ({data["voltage"]} V)")
+        print()
+    except (Exception,) as e:
+        print(f"An exception of type {type(e).__name__} occured")
+        print(e)
