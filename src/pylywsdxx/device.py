ddb28339f4301a9d5c4297c4f8784dba06224673
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-05-22 07:52:46 +0200

    fix stale connections causing connect errors

--- src/pylywsdxx/device.py
+++ src/pylywsdxx/device.py
@@ -208,20 +208,20 @@ class Lywsd02:  # pylint: disable=R0902
                     message = f"Device ({self._mac}) timed out on connect."
                     reraise = PyLyTimeout(f"-- {her} --")
                 LOGGER.warning(f"{message}")
-                # self._tries -= 1
-                # if self._tries <= 0:
-                # self._resets -= 1
-                # # ble_reset(debug=self.debug)
-                # self._set_tries()
-                # if self._resets <= 0:
-                #     # re-raise because apparently resetting the radio doesn't work
                 raise reraise from her
             except Exception as her:
                 # Non-anticipated exceptions must be raised to draw attention to them
-                # We'll reset the radio because it has had results in the past
                 message = f"Unexpected exception occured for device ({self._mac})."
                 reraise = PyLyException(f"-- {her} --")
                 LOGGER.error(f"{message}")
+                # Try to disconnect to avoid stale connections causing BTLEConnectError later.
+                if self.debug:
+                    print(f"|-< Disconnecting from {self._mac}")
+                    LOGGER.debug(f"|-< Disconnecting from {self._mac}")
+                try:
+                    self._peripheral.disconnect()
+                except Exception as her2:
+                    LOGGER.error(f"While disconnecting : {her}")
                 raise reraise from her
 
         self._context_depth += 1
