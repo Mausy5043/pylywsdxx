4482048e27f3c8efc0b0f52262609404725446bb
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-05-23 22:07:27 +0200

    log warning

--- src/pylywsdxx/device.py
+++ src/pylywsdxx/device.py
@@ -225,8 +225,8 @@ class Lywsd02:  # pylint: disable=R0902
                 # Try to disconnect to avoid stale connections causing BTLEConnectError later.
                 if self.debug:
                     print(f"|-< Disconnecting from {self._mac}  (forced_unk)")
-                    LOGGER.debug(f"|-< Disconnecting from {self._mac}  (forced_unk)")
                 try:
+                    LOGGER.warning(f"*** Disconnecting from {self._mac}  (forced_unk)")
                     self._peripheral.disconnect()
                 except Exception as her2:  # pylint: disable=broad-exception-caught
                     LOGGER.error(f"While disconnecting : {her2}")
