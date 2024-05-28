cd5ec354678faec1c53f04742588d3666fcc0c0c
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-05-28 08:33:53 +0200

    also restart service

--- src/pylywsdxx/radioctl.py
+++ src/pylywsdxx/radioctl.py
@@ -17,16 +17,17 @@ def ble_reset(delay: float = 20.0, debug: bool = False) -> tuple[str, str]:
     Args:
         delay (float): time [s] to wait between switching off and back on again.
         debug (bool): whether to provide debugging information.
-
     """
+    if debug:
+        LOGGER.level = logging.DEBUG
+
+    # fetch state of devices from bluetoothctl
     args: list[str] = ["/usr/bin/bluetoothctl", "devices"]
     _devices: str = subprocess.check_output(args, shell=False).decode(encoding="utf-8")  # nosec B603
     if debug:
         print(f"Known devices: {_devices}")
     LOGGER.info(f"Known devices: {_devices}")
 
-    if debug:
-        LOGGER.level = logging.DEBUG
     LOGGER.warning("Resetting BT-radio.")
 
     # Have you tried turning it off and on again?
@@ -35,7 +36,6 @@ def ble_reset(delay: float = 20.0, debug: bool = False) -> tuple[str, str]:
     if debug:
         print(f"Radio off ({_exit_code_on})")
     LOGGER.info(f"Radio off ({_exit_code_on})")
-
     time.sleep(delay)
     args = ["/usr/bin/bluetoothctl", "power", "on"]
     _exit_code_off: str = subprocess.check_output(args, shell=False).decode(encoding="utf-8")  # nosec B603
@@ -44,7 +44,11 @@ def ble_reset(delay: float = 20.0, debug: bool = False) -> tuple[str, str]:
     LOGGER.info(f"Radio on ({_exit_code_off})")
 
     # if all else fails...
-    # args = ["/usr/bin/sudo", "/usr/bin/systemctl", "restart", "bluetooth.service"]
-    # restart_result: str = subprocess.check_output(args, shell=False).decode(encoding="utf-8")  # nosec B603
+    args = ["/usr/bin/sudo", "/usr/bin/systemctl", "restart", "bluetooth.service"]
+    _restart_result: str = subprocess.check_output(args, shell=False).decode(encoding="utf-8")  # nosec B603
+    if debug:
+        print(f"Restarted bluetooth service ({_restart_result}")
+    LOGGER.info(f"Restarted bluetooth service ({_restart_result}")
+
     return (_exit_code_on, _exit_code_off)
 # fmt: on
